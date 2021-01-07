376553d05ba6dc777957a4076ed47ac0ea04abbe
Mateusz Dudzinski <dudzinskimat@outlook.com>
2021-01-07 18:34:05 +0100

    Smaller benchmarks

--- run-bench.sh
+++ run-bench.sh
@@ -2,5 +2,7 @@
 
 # LD_PRELOAD=../../sandbox/mimalloc/out/release/libmimalloc.so valgrind --tool=callgrind ./latc ./stress-test.lat
 GPRO2DOT=/home/mateusz/.local/bin/gprof2dot
+
+valgrind --tool=callgrind ./latc ./stress-test-1000.lat
 $(GPRO2DOT) --format=callgrind --output=out-mimalloc.dot ./callgrind.out.43190
 dot -Tsvg out-mimalloc.dot -o graph-mimalloc.svg
